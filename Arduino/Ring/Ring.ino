e77822cf991880c41465c186aa4891e6291afca4
Levi Johanon <ljohanon06@gmail.com>
2026-04-22 16:36:06 -0500

    Add Ring example code

---
+++ Arduino/Ring/Ring.ino
@@ -0,0 +1,52 @@
+#include <Adafruit_NeoPixel.h>
+#include <ESP32Servo.h>
+
+#define BUTTON_PIN D5
+
+#define RING_PIN 17
+#define LED_COUNT 12
+
+Adafruit_NeoPixel ring(LED_COUNT, RING_PIN, NEO_GRB + NEO_KHZ800); //Ring is transmitted Green, Red, Blue at 800 kHz
+
+unsigned long count = 0;
+
+volatile bool button_pressed = false;
+
+void IRAM_ATTR button_ISR(){ //ISR = Interrupt Service Routine
+    button_pressed = true;
+}
+
+void alarm_ring();
+void calm_ring();
+
+void setup() {
+  ring.begin();   //Initializes ring
+  ring.show();    //Pushes current LED setup to ring, as nothing has been setup yet, LED is blank
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN),button_ISR,FALLING);
+
+}
+
+void loop() {
+  if(button_pressed){
+    alarm_ring();
+  }else{
+    calm_ring();
+  }
+
+  count++;
+  delay(10);
+}
+
+void alarm_ring(){
+  long count_div = (count/100)%10; //Change every second
+
+  ring.fill(ring.Color(255,255-count_div*25,255-count_div*25),0,12);
+}
+
+void calm_ring(){
+  long count_div = (count/10)%100; //Change every 100 ms
+
+  ring.fill(ring.Color(255-count_div*2.55f,255,255-count_div*2.55f),0,12);
+}
