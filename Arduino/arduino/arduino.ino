1b75552ad935abe6ff825a7915be7ca979f15537
Levi Johanon <ljohanon06@gmail.com>
2026-04-22 16:09:01 -0500

    Arduino Code

--- Arduino/arduino/arduino.ino
+++ Arduino/arduino/arduino.ino
@@ -1,21 +1,24 @@
 #include <Adafruit_NeoPixel.h>
 #include <ESP32Servo.h>
 
-#define LED_PIN 1       //Pin on arduino
+#define RING_PIN 17     //D8
 #define LED_COUNT 12    //Number of leds on ring
 
-#define SERVO_PIN 2     //Pin on arduino
-#define BUZZER_PIN 3    //Pin on arduino
+#define SERVO_PIN 7     //D4
+#define BUZZER_PIN D5    //Pin on arduino
 
-#define TRIGGER_PIN 4
-#define ECHO_PIN 5
+#define TRIGGER_PIN A5
+#define ECHO_PIN A6
 
-#define LED_PIN 6
-#define BUTTON_PIN 7
+#define LED_PIN D2       //D2
+#define BUTTON_PIN D5    //D5
 
-Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800); //Ring is transmitted Green, Red, Blue at 800 kHz
+Adafruit_NeoPixel ring(LED_COUNT, RING_PIN, NEO_GRB + NEO_KHZ800); //Ring is transmitted Green, Red, Blue at 800 kHz
 
 Servo servo;
+
+long duration;
+float distanceCm;
 
 volatile bool button_pressed = false;
 
@@ -33,18 +36,23 @@ void setup(){
     ring.show();
 
     servo.setPeriodHertz(50);               //Servos run at 50 Hz
-    servo.attach(SERVO_PIN, 1000, 2000);    //The servo sends pulses for between 1000 and 2000 microseconds.
+    servo.attach(SERVO_PIN, 500, 2400);    //The servo sends pulses for between 1000 and 2000 microseconds.
 
+    servo.write(180);
+    delay(2000);
     servo.write(0);
-    delay(1000);
-    servo.write(90);
-    delay(1000);
+    delay(2000);
     servo.write(180);
 
     pinMode(LED_PIN, OUTPUT);
     pinMode(BUTTON_PIN, INPUT_PULLUP);
 
     attachInterrupt(digitalPinToInterrupt(BUTTON_PIN),button,FALLING);
+
+    pinMode(TRIGGER_PIN,OUTPUT);
+    pinMode(ECHO_PIN,INPUT);
+
+    Serial.begin(9600);
 }
 
 void loop(){
@@ -54,5 +62,22 @@ void loop(){
         digitalWrite(LED_PIN, LOW);
         button_pressed = false;
     }
+
+    digitalWrite(TRIGGER_PIN, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(TRIGGER_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER_PIN, LOW);
+
+    duration = pulseIn(ECHO_PIN, HIGH);
+
+    distanceCm = duration * 0.0343 / 2;
+
+    Serial.print("Distance: ");
+    Serial.print(distanceCm);
+    Serial.println(" cm");
+
+    delay(1000);
 }
 
