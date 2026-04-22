33e04ff344c4e3f4a57c49c5b0f7484d706ed735
Levi Johanon <ljohanon06@gmail.com>
2026-04-22 11:06:27 -0500

    Update arduino code

--- Arduino/arduino/arduino.ino
+++ Arduino/arduino/arduino.ino
@@ -11,20 +11,48 @@
 #define ECHO_PIN 5
 
 #define LED_PIN 6
-#define BUTTON_Pin 7
+#define BUTTON_PIN 7
 
 Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800); //Ring is transmitted Green, Red, Blue at 800 kHz
 
 Servo servo;
 
+volatile bool button_pressed = false;
+
+void IRAM_ATTR button(){
+    button_pressed = true;
+}
+
 void setup(){
     ring.begin();   //Initializes ring
     ring.show();    //Pushes current LED setup to ring, as nothing has been setup yet, LED is blank
 
+    for(int i = 0; i<12; i++){
+        ring.setPixelColor(i,10*i,120-10*i,0);
+    }
+    ring.show();
+
     servo.setPeriodHertz(50);               //Servos run at 50 Hz
     servo.attach(SERVO_PIN, 1000, 2000);    //The servo sends pulses for between 1000 and 2000 microseconds.
+
+    servo.write(0);
+    delay(1000);
+    servo.write(90);
+    delay(1000);
+    servo.write(180);
+
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN),button,FALLING);
 }
 
 void loop(){
-    
+    if(button_pressed){
+        digitalWrite(LED_PIN, HIGH);
+        delay(1000);
+        digitalWrite(LED_PIN, LOW);
+        button_pressed = false;
+    }
 }
+
